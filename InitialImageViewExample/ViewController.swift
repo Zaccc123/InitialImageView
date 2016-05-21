f554cd497063220b3188d9c82fc6fde0391ec818
Zac Kwan <weihao09@gmail.com>
2016-05-21 15:16:56 +0800

    Initial Commit

---
+++ InitialImageViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InitialImageViewExample
+//
+//  Created by Zac on 21/5/16.
+//  Copyright © 2016 Zac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
